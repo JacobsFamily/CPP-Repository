198dc65a1cea56391b7bffa756844e0c507d7116
alcubierrevj <alcubierrevj@gmail.com>
2015-05-10 23:43:24 -0400

    Create LotteryQuadruplet.h

---
+++ NBALottery/LotteryQuadruplet.h
@@ -0,0 +1,25 @@
+/*
+ * LotteryQuadruplet.h
+ *
+ *  Created on: Apr 18, 2014
+ *      Author: owner
+ */
+
+#ifndef LOTTERYQUADRUPLET_H_
+#define LOTTERYQUADRUPLET_H_
+
+namespace NBALottery {
+  class Quadruplet
+  {
+    public:
+	    Quadruplet();
+	    Quadruplet(int a, int b, int c, int d);
+	    ~Quadruplet();
+	    int num[4];
+  };
+
+
+}
+
+
+#endif /* LOTTERYQUADRUPLET_H_ */
