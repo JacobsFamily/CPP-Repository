40adc9c5a4203b2f154baecb4eaa1f5f3c891d9a
alcubierrevj <alcubierrevj@gmail.com>
2015-05-26 12:33:46 -0400

    Update Main.cc

--- Hmm/Main.cc
+++ Hmm/Main.cc
@@ -15,7 +15,7 @@ int main()
 {
 	Vec position(1.0, 2.0, 3.0);
 	Vec velocity(4.0, 5.0, 6.0);
-	Ball aBall(14,position, velocity);
+	Ball aBall(14,1.0,position, velocity);
 
 	cout << " this ball's number is: " << aBall.Num << endl;
 	cout << " this ball's Y velocity is: " << aBall.vel.y << endl;
