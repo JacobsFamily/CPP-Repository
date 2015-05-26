f01603a763398922d995d3046560bca8b5e17818
alcubierrevj <alcubierrevj@gmail.com>
2015-05-26 11:07:58 -0400

    Create SimEngine.h

---
+++ Hmm/SimEngine.h
@@ -0,0 +1,16 @@
+#ifndef SIMENGINE_H_
+#define SIMENGINE_H_
+#include "Force.h"
+#include "Ball.h"
+#include "Wall.h"
+
+class SimEngine
+{
+  public:
+    propagate(std::vector <Ball&> objects);
+    collide(Ball& thisBall, Ball& thatBall);
+    bounce(Ball& ball, Wall& wall);
+  private:
+    std::vector <Force> forces;
+};
+#endif
