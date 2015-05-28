406b3b252f592484d456dcb4f5e01286c2dc4950
alcubierrevj <alcubierrevj@gmail.com>
2015-05-28 14:58:49 -0400

    Maybe the alignment will stick this time.

--- Hmm/Ball.cc
+++ Hmm/Ball.cc
@@ -10,20 +10,20 @@
 Ball::Ball()
      :Num(0),
       mass(0.0),
- 	    pos(0.0,0.0,0.0),
- 	    vel(0.0,0.0,0.0)
+      pos(0.0,0.0,0.0),
+      vel(0.0,0.0,0.0)
 {}
 
 Ball::Ball(int inNum, double inMass, double inX, double inY, double inZ, double inVX, double inVY, double inVZ)
      :Num(inNum),
-       mass(inMass),
-  	   pos(inX,inY,inZ),
-  	   vel(inVX,inVY,inVZ)
+      mass(inMass),
+      pos(inX,inY,inZ),
+      vel(inVX,inVY,inVZ)
 {}
 
 Ball::Ball(int inNum, double inMass, Vec inPos, Vec inVel)
      :Num(inNum),
-	    mass(inMass),
-	    pos(inPos),
-	    vel(inVel)
+      mass(inMass),
+      pos(inPos),
+      vel(inVel)
 {}
