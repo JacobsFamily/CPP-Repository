e8385171ac7f897311a366073a3b98b84234dc76
alcubierrevj <alcubierrevj@gmail.com>
2015-05-28 14:56:56 -0400

    Replaced tabs with spaces, aligned initializers

--- Hmm/Ball.cc
+++ Hmm/Ball.cc
@@ -7,22 +7,23 @@
 
 #include "Ball.h"
 
-Ball::Ball():
-           Num(0),
-           mass(0.0),
- 	       pos(0.0,0.0,0.0),
- 	       vel(0.0,0.0,0.0)
-{}
-Ball::Ball(int inNum, double inMass, double inX, double inY, double inZ, double inVX, double inVY, double inVZ):
-           Num(inNum),
-           mass(inMass),
-  	       pos(inX,inY,inZ),
-  	       vel(inVX,inVY,inVZ)
+Ball::Ball()
+     :Num(0),
+      mass(0.0),
+ 	    pos(0.0,0.0,0.0),
+ 	    vel(0.0,0.0,0.0)
 {}
 
-Ball::Ball(int inNum, double inMass, Vec inPos, Vec inVel):
-	   Num(inNum),
-	   mass(inMass),
-	   pos(inPos),
-	   vel(inVel)
+Ball::Ball(int inNum, double inMass, double inX, double inY, double inZ, double inVX, double inVY, double inVZ)
+     :Num(inNum),
+       mass(inMass),
+  	   pos(inX,inY,inZ),
+  	   vel(inVX,inVY,inVZ)
+{}
+
+Ball::Ball(int inNum, double inMass, Vec inPos, Vec inVel)
+     :Num(inNum),
+	    mass(inMass),
+	    pos(inPos),
+	    vel(inVel)
 {}
